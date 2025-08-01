2fdc78a391a22c0a3396cf36410f4ef5f1600411
Somnang Rath <somnang.rath12@gmail.com>
2025-08-02 00:13:07 +0700

    Create model,controller and migrate

---
+++ database/migrations/2025_08_01_164923_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id(); // auto key
+            $table->string('movie_image');
+            $table->string('title_movie');
+            $table->string('screen_type');
+            $table->string('genre');
+            $table->string('duration');
+            $table->string('release');
+            $table->string('classification');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
---
+++ database/migrations/2025_08_01_165005_create_offers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id(); // auto key
+            $table->string('img')->nullable();
+            $table->string('title');
+            $table->string('date_time');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('offers');
+    }
+};
